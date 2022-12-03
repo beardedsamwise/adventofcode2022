ed0b0bdaf7c6c884c6117585ad60bc973f2df60e
Sam Bentley <sam.bentley@contino.io>
2022-12-04 09:58:02 +1100

    part 2 complete

--- 2/rockpaperscissors.py
+++ 2/rockpaperscissors.py
@@ -7,27 +7,48 @@ strategy = np.loadtxt(filename, delimiter=' ', dtype=str)
 score = 0
 
 # convert rock, paper, scissors moves into integers for simple comparison
-win_dict = {"A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
-
+comparison_dict = {"A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
 
 for move in strategy:
-        their_move = win_dict.get(move[0])
-        our_move = win_dict.get(move[1])
-        # their winning moves - rock beatss scissors + paper beats rock + scissors beats paper
+        their_move = comparison_dict.get(move[0])
+        our_move = comparison_dict.get(move[1])
         if (their_move == 1 and our_move == 3) or (their_move == 2 and our_move == 1) or (their_move == 3 and our_move == 2):
             round_score = our_move + 0 #lose 
             score += round_score
-        # draw
         elif their_move == our_move:
             round_score = our_move + 3 #draw
             score += round_score
-        # otherwise we win
         else:
             round_score = our_move + 6 #win
             score += round_score
 
+print("Part 1: " + str(score))
 
-print(score)
+# reset score to 0
+score = 0
 
+# dictionary of moves
+lose_dict = {"A":"Z", "B":"X", "C":"Y"}
+draw_dict = {"A":"X", "B":"Y", "C":"Z"}
+win_dict  = {"A":"Y", "B":"Z", "C":"X"}
+
+for move in strategy:  
+        if move[1] == "X": 
+            our_move = lose_dict.get(move[0]) # get our moved based on their move
+            our_move = comparison_dict.get(our_move) # get score for our move
+            round_score = our_move + 0 #lose 
+            score += round_score
+        elif move[1] == "Y":
+            our_move = draw_dict.get(move[0])
+            our_move = comparison_dict.get(our_move)
+            round_score = our_move + 3 #draw
+            score += round_score
+        else:
+            our_move = win_dict.get(move[0])
+            our_move = comparison_dict.get(our_move)
+            round_score = our_move + 6 #win
+            score += round_score
+
+print("Part 2: " + str(score))
 
 
