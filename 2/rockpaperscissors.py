e3f24b7ca5ec7d36b19a78a452b9d8311d9d4be9
Sam Bentley <sam.bentley@contino.io>
2022-12-04 09:34:41 +1100

    finish day 2 part 1

--- 2/rockpaperscissors.py
+++ 2/rockpaperscissors.py
@@ -1,7 +1,7 @@
 # import strategy
 import numpy as np
 filename = 'input.txt'
-strategy = np.loadtxt(filename, delimiter=' ', skiprows=1, dtype=str)
+strategy = np.loadtxt(filename, delimiter=' ', dtype=str)
 
 # set score at 0
 score = 0
@@ -9,37 +9,23 @@ score = 0
 # convert rock, paper, scissors moves into integers for simple comparison
 win_dict = {"A" : 1, "B" : 2, "C" : 3, "X" : 1, "Y" : 2, "Z" : 3}
 
-def get_move(move):
-    if move == "X":
-        value = win_dict.get("X")
-    elif move == "Y":
-        value = win_dict.get("Y")
-    elif move == "Z":
-        value = win_dict.get("Z")     
-    elif move == "A":
-        value = win_dict.get("A")
-    elif move == "B":
-        value = win_dict.get("B")   
-    else:
-        value = win_dict.get("C")
-    return value
-
 
 for move in strategy:
-        their_move = get_move(move[0])
-        our_move = get_move(move[1])
-        if their_move > our_move:
+        their_move = win_dict.get(move[0])
+        our_move = win_dict.get(move[1])
+        # their winning moves - rock beatss scissors + paper beats rock + scissors beats paper
+        if (their_move == 1 and our_move == 3) or (their_move == 2 and our_move == 1) or (their_move == 3 and our_move == 2):
             round_score = our_move + 0 #lose 
-            print(str(round_score) + " lose " + str(our_move))
             score += round_score
-        elif their_move < our_move:
-            round_score = our_move + 6 #win
-            print(str(round_score) + " win " + str(our_move))
-            score += round_score
+        # draw
         elif their_move == our_move:
             round_score = our_move + 3 #draw
-            print(str(round_score) + " draw " + str(our_move))
             score += round_score
+        # otherwise we win
+        else:
+            round_score = our_move + 6 #win
+            score += round_score
+
 
 print(score)
 
