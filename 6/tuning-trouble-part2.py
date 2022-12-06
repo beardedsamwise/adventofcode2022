21fba0b3b402164e6cf690d2ed037b22b9578014
Sam Bentley <sam.bentley@contino.io>
2022-12-07 10:02:00 +1100

    part 2 complete

---
+++ 6/tuning-trouble-part2.py
@@ -0,0 +1,18 @@
+signal = open('input.txt', 'r').read()
+
+def checkIfDuplicates(listOfElems):
+    ''' Check if given list contains any duplicates '''
+    if len(listOfElems) == len(set(listOfElems)):
+        return False
+    else:
+        return True
+
+for i in range(len(signal)):
+    current_signal = []
+    for each in signal[i:i+14]:
+        current_signal.append(each)
+    result = checkIfDuplicates(current_signal)
+    if not result:
+        print('Marker location: ' + str(i + 14))
+        print(current_signal)
+        break
