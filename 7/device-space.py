e8eb044edd95595b88b0299b7abfd4fe9acfde01
Sam Bentley <sam.bentley@contino.io>
2022-12-16 15:19:27 +1100

    part 2 solved

--- 7/device-space.py
+++ 7/device-space.py
@@ -35,28 +35,40 @@ for output in puzzle:
 
 # calculate file sizes
 tally = 0
+dir_totals = {}
 for dir_par,sizes_par in size_tree.items():
-    if dir_par == "/": # skip the root dir, even though it's probably too big
-        continue
-    else:
-        print("Processing " + dir_par)
+    # if dir_par == "/": # skip the root dir, even though it's probably too big
+    #     continue
+    # else:
+        #print("Processing " + dir_par)
         size = 0
         for dir_child,sizes_child in size_tree.items():
             if dir_par == dir_child: # add the current directory to size
                 size += sum(sizes_child)
-                print("Adding " + dir_par)
+                #print("Adding " + dir_par)
             elif dir_child.startswith(dir_par): # check if other dirs are child directories, if so add to size
-                print("Adding " + dir_child + " to tally.")
+                #print("Adding " + dir_child + " to tally.")
                 size += sum(sizes_child)
-        print("Size: " + str(size))
+        #print("Size: " + str(size))
         if size <= 100000: # if size of parent + child dirs is under threshold, add to tally
-            print("Directory is under the threshold, adding...")
+            #print("Directory is under the threshold, adding...")
             tally += size
-        else:
-            print("Too big... skipping...")
+        # else:
+        #     #print("Too big... skipping...")
+        dir_totals[dir_par] = size
 
-print(tally)
 
+
+size = []
+unused_space = 70000000 - dir_totals['/']
+needed_space = 30000000 - unused_space
+
+for k,v in dir_totals.items():
+    if v >= needed_space:
+        size.append(v)
+
+print("Part 1: " + str(tally))
+print("Part 2: " + str(min(size)))
 
 
 
