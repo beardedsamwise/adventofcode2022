1a68bfc3cab0fd38c6bb9145170ce078e5983bee
Sam Bentley <sam.bentley@contino.io>
2022-12-20 09:30:22 +1100

    refactor code

--- 8/treeTopHouse.py
+++ 8/treeTopHouse.py
@@ -3,16 +3,11 @@ data = input.read()
 input.close()
 forest = data.split("\n")
 
-x = 3
-y = 1
-
 visible_tree = 0
 
 # add outer rim trees
-visible_tree += len(forest) * 2
-visible_tree += (len(forest[0]) - 2 ) * 2
-
-print(visible_tree)
+visible_tree += len(forest) * 2 # add vertical trees 
+visible_tree += (len(forest[0]) - 2 ) * 2 # add horizontal trees minus corner trees already counted
 
 def innerTrees(input_row, input_column):
     row = forest[input_row - 1] # create string containing all items in the trees row 
@@ -22,39 +17,28 @@ def innerTrees(input_row, input_column):
         column += str(each[input_column - 1])
 
     # init row and column sight, assume sight is true to begin with
-    row_sight = False 
-    column_sight = False
+    sight = False
 
-    # check each row and column for a taller tree, skip the tree we're actually checking, if a taller tree is found set sight to false
     # check left
     for i in range(0, (x - 1)):
         if not row[i] >= tree:
-            row_sight = True
+            sight = True
     # check right
     for i in range((x + 1), len(row) - 1):
         if not row[i] >= tree:
-            row_sight = True
+            sight = True
     # check above
     for i in range(0, (y - 1)):
         if not column[i] >= tree:
-            column_sight = True
+            sight = True
     # check below
     for i in range((y + 1), len(column) - 1):
         if not column[i] >= tree:
-            column_sight = True
-    # print(row)
-    # print(column)
-    # print(tree)
-    # print("Row sight: " + str(row_sight))
-    # print("Column sight: " + str(column_sight))
-    if row_sight or column_sight:
-        return True
-    else:   
-        return False
+            sight = True
+    return sight
             
 for x in range(1, (len(forest) - 1)):
         for y in range(1, (len(forest[0]) - 1)):
-            #print(str(x) + " " + str(y))
             sight = innerTrees(x , y)
             if sight == True:
                 visible_tree += 1
