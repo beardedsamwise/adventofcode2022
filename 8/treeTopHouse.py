8984a6e248a38bb5c1dc16138e29589c8c673d6b
Sam Bentley <sam.bentley@contino.io>
2022-12-20 09:28:14 +1100

    wip - example answer OK

--- 8/treeTopHouse.py
+++ 8/treeTopHouse.py
@@ -1,34 +1,62 @@
 input = open("input.txt", "r")
 data = input.read()
 input.close()
-data_into_list = data.split("\n")
+forest = data.split("\n")
 
 x = 3
 y = 1
 
-def test(input_row, input_column):
-    row = data_into_list[input_row - 1]
-    tree = row[input_column - 1]
-    column = str("")
-    for each in data_into_list:
-        column += str(each[input_column - 1])
-    row_sight = True
-    column_sight = True
-    for i in range(len(row)):
-        if i == int(input_row) - 1:
-            continue
-        elif each >= tree:
-            row_sight = False
-    for i in range(len(column)):
-        if i == int(input_column) - 1:
-            continue
-        elif each >= tree:
-            column_sight = False
-    print(row)
-    print(column)
-    print(tree)
-    print("Row sight: " + str(row_sight))
-    print("Column sight: " + str(column_sight))
-            
+visible_tree = 0
 
-test(x, y)
+# add outer rim trees
+visible_tree += len(forest) * 2
+visible_tree += (len(forest[0]) - 2 ) * 2
+
+print(visible_tree)
+
+def innerTrees(input_row, input_column):
+    row = forest[input_row - 1] # create string containing all items in the trees row 
+    tree = row[input_column - 1] # get the actual tree we are checking
+    column = str("") # init empty string for column of trees
+    for each in forest: # create new string containing column of trees
+        column += str(each[input_column - 1])
+
+    # init row and column sight, assume sight is true to begin with
+    row_sight = False 
+    column_sight = False
+
+    # check each row and column for a taller tree, skip the tree we're actually checking, if a taller tree is found set sight to false
+    # check left
+    for i in range(0, (x - 1)):
+        if not row[i] >= tree:
+            row_sight = True
+    # check right
+    for i in range((x + 1), len(row) - 1):
+        if not row[i] >= tree:
+            row_sight = True
+    # check above
+    for i in range(0, (y - 1)):
+        if not column[i] >= tree:
+            column_sight = True
+    # check below
+    for i in range((y + 1), len(column) - 1):
+        if not column[i] >= tree:
+            column_sight = True
+    # print(row)
+    # print(column)
+    # print(tree)
+    # print("Row sight: " + str(row_sight))
+    # print("Column sight: " + str(column_sight))
+    if row_sight or column_sight:
+        return True
+    else:   
+        return False
+            
+for x in range(1, (len(forest) - 1)):
+        for y in range(1, (len(forest[0]) - 1)):
+            #print(str(x) + " " + str(y))
+            sight = innerTrees(x , y)
+            if sight == True:
+                visible_tree += 1
+
+print(visible_tree)
